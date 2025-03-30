dc085dcc44a69a9c488c6bb5a9ae02fb8d0f814e
Lucas <kucasss19@gmail.com>
2025-03-30 08:20:53 -0300

    feat: add react js for front-end

---
+++ website/control-iot-project/src/Home.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Home() {
+
+  return (
+
+    <div className="text">
+      <h1>Bem-vindo ao website do controle do IoT</h1>
+    </div>
+
+  );
+
+}
+
+export default Home
